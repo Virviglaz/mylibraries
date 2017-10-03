38c34aae32fb1eae7e109f389477a2d56f5d45a9
Pavel <pasha-net@narod.ru>
2017-10-03 21:45:12 +0200

    большие изменения в console

--- Common/console.c
+++ Common/console.c
@@ -1,44 +1,52 @@
 #include "console.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-char ** GetParamsFromString (char * src, char delim)
+char ** ConsoleGetPars (char * src, char * delim)
 {
 	char ** result; //pointer to pointer value that will be returned as the result
 	char * token;
-	char delimstring[] = {delim, 0};
 	uint8_t i = 0;
 
 	/* Calculate amount of memory need to store params pointers */
 	token = src; //store pointer position
 	while(*src)
-		if (*src++ == delim)
+		if (strcmp(src++, delim) == 0)
 			i++;
 	src = token; //restore pointer position (reusing variable)
 		
 	/* Get memory for array of pointers */
-	result = malloc(i + 2);
+	result = malloc((i + 2) * sizeof(char *));
 		
 	result[i + 1] = NULL; //null terminate last pointer
 	i = 0;	//reusing counter variable
 		
 	/* Find first occurance */	
-	token = strtok(src, delimstring);
+	token = strtok(src, delim);
 		
 	/* While possible, assing the pointer values */	
 	while (token)
 	{
 		result[i++] = token; //store pointer position
-		token = strtok(NULL, delimstring); //get new occurance if possible
+		token = strtok(NULL, delim); //get new occurance if possible
 	}
 	
 	return result;
 }
 
-size_t GetParamsFromStringLen (char ** params)
+size_t ConsoleGetParsLen (char ** params)
 {
 	size_t i = 0;
 	while (params[i++]);
 	return i - 1;
+}
+
+size_t ConsoleGetParsNum (char * src, char * delim)
+{
+	size_t i = 0;
+
+	while(*src)
+		if (strcmp(src++, delim) == 0)
+			i++;
+
+	return i;
 }
 
--- Common/console.h
+++ Common/console.h
@@ -1,7 +1,20 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
-char ** GetParamsFromString (char * src, char delim);
-size_t GetParamsFromStringLen (char ** params);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+char ** ConsoleGetPars (char * src, char * delim);
+size_t ConsoleGetParsLen (char ** params);
+size_t ConsoleGetParsNum (char * src, char * delim);
+
+static const struct
+{
+	char ** (* GetPars) (char * src, char * delim);
+	size_t (* GetParsLen) (char ** params);
+	size_t (* GetParsNum) (char * src, char * delim);
+}Console = { ConsoleGetPars, ConsoleGetParsLen, ConsoleGetParsNum };
 
 #endif
