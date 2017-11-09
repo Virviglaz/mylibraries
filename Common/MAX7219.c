5284ef53734f6b9983e26fbd02d21e2d79f59f45
Pavel <pavelnadein@gmail.com>
2017-11-09 23:20:27 +0200

    MAX7219 добавлено несколько функций

--- Common/MAX7219.c
+++ Common/MAX7219.c
@@ -1,37 +1,140 @@
 #include "MAX7219.h"
 
+#define CHIP_ENABLE  0
+#define CHIP_DISABLE 1
+
 /* Private functions  declaration */
 uint8_t GetDeviceN (MAX7219_InitStructTypeDef * MAX7219_InitStruct);
+void WriteReg (MAX7219_InitStructTypeDef * MAX7219_InitStruct, uint8_t Reg, uint8_t Value);
+uint8_t reverse(uint8_t b);
 
 /* Public functions */
 uint8_t MAX7219_Init (MAX7219_InitStructTypeDef * MAX7219_InitStruct)
+{        
+  if (MAX7219_InitStruct->DeviceN == 0) //autodetect amount of devices on bus if not defined
+    MAX7219_InitStruct->DeviceN = GetDeviceN(MAX7219_InitStruct);
+	
+  WriteReg(MAX7219_InitStruct, MAX7219_REG_DecodeMode, (uint8_t)MAX7219_InitStruct->DecodeMode);
+  WriteReg(MAX7219_InitStruct, MAX7219_REG_Intensity, (uint8_t)MAX7219_InitStruct->Intensity);
+  WriteReg(MAX7219_InitStruct, MAX7219_REG_ScanLim, (uint8_t)MAX7219_InitStruct->ScanLimit);
+  WriteReg(MAX7219_InitStruct, MAX7219_REG_TestMode, (uint8_t)MAX7219_InitStruct->TestMode);
+  WriteReg(MAX7219_InitStruct, MAX7219_REG_ShutDown, (uint8_t)MAX7219_InitStruct->Power);
+  
+   return 0;
+}
+
+void MAX7219_Print (MAX7219_InitStructTypeDef * MAX7219_InitStruct, uint8_t * buf, uint8_t * font)
 {
-	if (MAX7219_InitStruct->DeviceN == 0) //autodetect amount of devices on bus if not defined
-		MAX7219_InitStruct->DeviceN = GetDeviceN(MAX7219_InitStruct);
-	
-	
+  uint8_t digcnt = 0;
+  while (* buf)
+    MAX7219_UpdateDigit(MAX7219_InitStruct, digcnt++, font + MAX7219_InitStruct->DigitsPerChip * (*buf++));
+}
+
+void MAX7219_UpdateDigit (MAX7219_InitStructTypeDef * MAX7219_InitStruct, uint8_t DigNum, uint8_t * buf)
+{
+  uint8_t devcnt, digcnt;
+
+  for (digcnt = 0; digcnt != MAX7219_InitStruct->DigitsPerChip; digcnt++)
+  {
+    MAX7219_InitStruct->ChipEnable(CHIP_ENABLE);
+    for (devcnt = 0; devcnt != MAX7219_InitStruct->DeviceN; devcnt++)
+    {
+      if (devcnt == DigNum)
+      {
+        MAX7219_InitStruct->SendReceive(digcnt + 1);
+        MAX7219_InitStruct->xMirror ? MAX7219_InitStruct->SendReceive(reverse(*buf++)) : MAX7219_InitStruct->SendReceive(*buf++);
+          
+      }
+      else
+      {
+        MAX7219_InitStruct->SendReceive(0); //NOP
+        MAX7219_InitStruct->SendReceive(0);
+      }
+    }
+    MAX7219_InitStruct->ChipEnable(CHIP_DISABLE);
+  }  
+}
+
+void MAX7219_DrawWindow (MAX7219_InitStructTypeDef * MAX7219_InitStruct, uint8_t * buf, uint16_t size)
+{
+  uint8_t devcnt, cnt, byte, row = 1;
+  while (size)
+  {
+    MAX7219_InitStruct->ChipEnable(CHIP_ENABLE);
+    for (devcnt = 0; devcnt != MAX7219_InitStruct->DeviceN; devcnt++)
+    {
+      byte = 0;
+      for (cnt = 0; cnt != 8; cnt++)
+      {
+        if (*buf++) byte |= (1 << cnt);
+        size--;
+      }
+      MAX7219_InitStruct->SendReceive(row);
+      MAX7219_InitStruct->SendReceive(byte);
+    }
+    MAX7219_InitStruct->ChipEnable(CHIP_DISABLE);
+    row++;
+  }
+}
+
+void MAX7219_StructInitForDecode (MAX7219_InitStructTypeDef * MAX7219_InitStruct)
+{
+  MAX7219_InitStruct->DecodeMode = 0xFF;
+  MAX7219_InitStruct->Intensity = 0x0F;
+  MAX7219_InitStruct->ScanLimit = 7;
+  MAX7219_InitStruct->TestMode = MAX7219_Test_NormalOperation;
+  MAX7219_InitStruct->Power = MAX7219_Power_NormalOperation; 
+}
+
+void MAX7219_StructInitNoDecode (MAX7219_InitStructTypeDef * MAX7219_InitStruct)
+{
+  MAX7219_InitStruct->DecodeMode = 0x00;
+  MAX7219_InitStruct->Intensity = 0x0F;
+  MAX7219_InitStruct->ScanLimit = 7;
+  MAX7219_InitStruct->TestMode = MAX7219_Test_NormalOperation;
+  MAX7219_InitStruct->Power = MAX7219_Power_NormalOperation; 
+}
+  
+void WriteReg (MAX7219_InitStructTypeDef * MAX7219_InitStruct, uint8_t Reg, uint8_t Value)
+{
+  uint8_t cnt;
+  
+  MAX7219_InitStruct->ChipEnable(CHIP_ENABLE);
+  for (cnt = 0; cnt != MAX7219_InitStruct->DeviceN; cnt++)
+  {
+    MAX7219_InitStruct->SendReceive(Reg);
+    MAX7219_InitStruct->SendReceive(Value);
+  }
+  MAX7219_InitStruct->ChipEnable(CHIP_DISABLE);
 }
 
 uint8_t GetDeviceN (MAX7219_InitStructTypeDef * MAX7219_InitStruct)
 {
-	uint8_t cnt = 0, rtn;
-	if (MAX7219_InitStruct->SendByte == 0) return 0;
-	
-	do
-	{
-		cnt++;
-		rtn = MAX7219_InitStruct->SendByte(0xFF);
-	}while (cnt < 100 && rtn != 0xFF)
-		
-	cnt = 0;
-	
-	do
-	{
-		cnt++;
-		rtn = MAX7219_InitStruct->SendByte(0x00);
-	}while (cnt < 100 && rtn != 0x00)
-		
-	return cnt;
+  uint8_t cnt = 0, rtn;
+  if (MAX7219_InitStruct->SendReceive == 0) return 0;
+  
+  do
+  {
+    cnt++;
+    rtn = MAX7219_InitStruct->SendReceive(0xFF);
+  }while (cnt < 100 && rtn != 0xFF);
+          
+  cnt = 0;
+  
+  do
+  {
+    cnt++;
+    rtn = MAX7219_InitStruct->SendReceive(0x00);
+  }while (cnt < 100 && rtn != 0x00);
+          
+  return cnt;
 }
 
+uint8_t reverse(uint8_t b) 
+{
+   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+   return b;
+}
 
--- Common/MAX7219.h
+++ Common/MAX7219.h
@@ -18,40 +18,41 @@ typedef enum
 	MAX7219_Test_DisplayTest
 }MAX7219_TestModeEnumTypeDef;
 
-typedef enum
-{
-	MAX7219_REG_Nop = 0x00,
-	MAX7219_REG_Dig0 = 0x01,
-	MAX7219_REG_Dig1 = 0x02,
-	MAX7219_REG_Dig2 = 0x03,
-	MAX7219_REG_Dig3 = 0x04,
-	MAX7219_REG_Dig4 = 0x05,
-	MAX7219_REG_Dig5 = 0x06,
-	MAX7219_REG_Dig6 = 0x07,
-	MAX7219_REG_Dig7 = 0x08,
-	MAX7219_REG_DecodeMode = 0x09,
-	MAX7219_REG_Intensity = 0x0A,
-	MAX7219_REG_ScanLim = 0x0B,
-	MAX7219_REG_ShutDown = 0x0C,
-	MAX7219_REG_TestMode = 0x0F
-}MAX7219_RegistersEnumTypeDef;
+#define	MAX7219_REG_Nop	        0x00
+#define	MAX7219_REG_Dig0	0x01
+#define	MAX7219_REG_Dig1	0x02
+#define	MAX7219_REG_Dig2	0x03
+#define	MAX7219_REG_Dig3	0x04
+#define	MAX7219_REG_Dig4	0x05
+#define	MAX7219_REG_Dig5	0x06
+#define	MAX7219_REG_Dig6	0x07
+#define	MAX7219_REG_Dig7	0x08
+#define	MAX7219_REG_DecodeMode	0x09
+#define	MAX7219_REG_Intensity	0x0A
+#define	MAX7219_REG_ScanLim	0x0B
+#define	MAX7219_REG_ShutDown	0x0C
+#define	MAX7219_REG_TestMode	0x0F
 
 typedef struct
 {
 	MAX7219_PowerEnumTypeDef Power;
 	MAX7219_TestModeEnumTypeDef TestMode;
-	uint8_t (*SendReceive) (uint8_t * Data, uint8_t size);
-	uint8_t (*SendByte)(uint8_t value);
+        uint8_t DecodeMode;
+        uint8_t Intensity;
+        uint8_t ScanLimit;
 	uint8_t DeviceN;
+        uint8_t DigitsPerChip;
+        uint8_t xMirror;
+        
+	uint8_t (*SendReceive)(uint8_t value);
+        void (*ChipEnable) (uint8_t value);
 }MAX7219_InitStructTypeDef;
 
-typedef struct
-{
-	uint8_t * Data;
-	uint8_t Size;
-	MAX7219_InitStructTypeDef * MAX7219_InitStruct;
-}MAX7219_StructTypeDef;
-
 uint8_t MAX7219_Init (MAX7219_InitStructTypeDef * MAX7219_InitStruct);
+void MAX7219_Print (MAX7219_InitStructTypeDef * MAX7219_InitStruct, uint8_t * buf, uint8_t * font);
+void MAX7219_UpdateDigit (MAX7219_InitStructTypeDef * MAX7219_InitStruct, uint8_t DigNum, uint8_t * buf);
+void MAX7219_DrawWindow (MAX7219_InitStructTypeDef * MAX7219_InitStruct, uint8_t * buf, uint16_t size);
+void MAX7219_StructInitForDecode (MAX7219_InitStructTypeDef * MAX7219_InitStruct);
+void MAX7219_StructInitNoDecode (MAX7219_InitStructTypeDef * MAX7219_InitStruct);
 
 #endif
