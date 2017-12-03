e23a8c530f1f1b94a67ea856fb4383169f79f384
Pavel <pavelnadein@gmail.com>
2017-12-03 22:44:34 +0200

    HD44780 драйвер переработан
    можно выводить свои символы

--- Common/HD44780.c
+++ Common/HD44780.c
@@ -8,9 +8,9 @@ HD44780_StructTypeDef * HD44780;
 static void (* HD44780_Write) (uint8_t data, uint8_t cmd);
 
 /* Internal functions protorypes */
+static void HD44780_WriteToBus (uint8_t dat);
 static void HD44780_Write_4bit (uint8_t data, uint8_t cmd);
 static void HD44780_Write_8bit (uint8_t data, uint8_t cmd);
-static void HD44780_Cmd(HD44780_CMD_TypeDef CMD);
 static uint8_t Translate (uint8_t dat);
 
 /* Public functions */
@@ -34,13 +34,16 @@ void HD44780_Init (HD44780_StructTypeDef * HD44780_InitStruct)
   HD44780_Write = HD44780->HD44780_Connection->BUS ? 
             HD44780_Write_8bit : HD44780_Write_4bit;
 
+	
   if (HD44780->HD44780_Connection->BUS == BUS_4b)
   {
-    HD44780_Write(0x03, 1);
-    HD44780_Write(0x03, 1);
-    HD44780_Write(0x03, 1);
-    HD44780_Write(0x02, 1);  //4 bit wide bus
+		/* 4 bit wide bus init */
+    HD44780_WriteToBus(0x03);
+    HD44780_WriteToBus(0x03);
+    HD44780_WriteToBus(0x03);
+    HD44780_WriteToBus(0x02);  
   }
+	else
 
   HD44780->Delay_Func(INIT_DELAY);	
   HD44780_Cmd((HD44780_CMD_TypeDef)(HD44780_CMD_4b_2lines | 
@@ -85,9 +88,9 @@ void HD44780_StdConnectionInit (HD44780_Connection_TypeDef * HD44780_Connection,
 
 void HD44780_SetPos (uint8_t row, uint8_t col)
 {
-        uint8_t position = 0x80;
-        if (row) position |= 0x40;
-        HD44780_Cmd ((HD44780_CMD_TypeDef)(position | col));
+	uint8_t position = 0x80;
+	if (row) position |= 0x40;
+	HD44780_Cmd ((HD44780_CMD_TypeDef)(position | col));
 }
 
 void HD44780_Print (char * string)
@@ -109,8 +112,7 @@ void HD44780_PutChar (uint8_t data)
   else 
   {
     data = Translate(data);
-    HD44780_Write(data & 0xF0, 0);
-    HD44780_Write((data & 0x0F) << 4, 0);
+    HD44780_Write(data, 0);
   }
 }
 
@@ -119,6 +121,22 @@ void HD44780_Update (void)
   HD44780_Cmd(HD44780_RAW_DATA);
 }
 
+void HD44780_Cmd (HD44780_CMD_TypeDef CMD)
+{
+  HD44780_Write(CMD, 1);
+}
+
+void HD44780_CustomChar (uint8_t num, uint8_t * data)
+{
+	uint8_t cnt;
+	HD44780_Cmd ((HD44780_CMD_TypeDef)(HD44780_CMD_SetCGRAM + num * 8));
+	for (cnt = 0; cnt != 8; cnt++)
+		HD44780_Write(data[cnt], 0);
+
+	HD44780_Cmd (HD44780_CMD_Home);	
+}
+
+/* Internal functions */
 static uint8_t Translate (uint8_t dat)
 {
   if (HD44780->Font != ENGLISH_RUSSIAN_FONT) return dat; //support only russian
@@ -127,12 +145,14 @@ static uint8_t Translate (uint8_t dat)
   return HD44780_RusFont[dat - 0xC0];
 }
 
-/* Internal functions */
 static void HD44780_Write_4bit (uint8_t data, uint8_t cmd)
 { 
   /* Prepare SPI data */
-  uint8_t LatchData = data << HD44780->HD44780_Connection->data_shift;
-  if (!cmd) LatchData = HD44780->HD44780_Connection->RS_Pin | data;
+  uint8_t LatchData = 0;
+	
+	if (cmd == HD44780_RAW_DATA) return; //do nothing
+	
+  if (!cmd) LatchData = HD44780->HD44780_Connection->RS_Pin;
 
   /* Set pin states */
   if (HD44780->BackLightIsOn)
@@ -142,25 +162,33 @@ static void HD44780_Write_4bit (uint8_t data, uint8_t cmd)
   if (HD44780->HD44780_ExtConnection)
     LatchData |= HD44780->HD44780_ExtConnection->EXT1_Pin
       | HD44780->HD44780_ExtConnection->EXT2_Pin;  
-  
-  /* Send data */	
-  if (cmd != HD44780_RAW_DATA)
-  {
-    /* Send positive strobe */
-    LatchData |= HD44780->HD44780_Connection->E_Pin;
-    HD44780->WriteData(LatchData);
 
-    /* Send negative strobe */
-    LatchData &= ~HD44780->HD44780_Connection->E_Pin;
-    HD44780->WriteData(LatchData);
-  }
+	/* Send high nible */
+	HD44780_WriteToBus(LatchData | ((data & 0xF0) >> 4) << HD44780->HD44780_Connection->data_shift);
+	
+	/* Send low nible */
+	HD44780_WriteToBus(LatchData | ((data & 0x0F) >> 0) << HD44780->HD44780_Connection->data_shift);
+}
+
+static void HD44780_WriteToBus (uint8_t dat)
+{
+	/* Send positive strobe */
+	dat |= HD44780->HD44780_Connection->E_Pin;
+	HD44780->WriteData(dat);
+
+	/* Send negative strobe */
+	dat &= ~HD44780->HD44780_Connection->E_Pin;
+	HD44780->WriteData(dat);
 }
 
 // data format MSB->DDDDDDDD->CCCCCCCC->LSB, C-ctrl, D-data pins
 static void HD44780_Write_8bit (uint8_t data, uint8_t cmd)
-{
+{	
   /* Backlight enable/disable and data prepare*/
   uint16_t LatchData = (data << 8) | HD44780->BackLightIsOn; 
+
+	if (cmd == HD44780_RAW_DATA) return;
+
   if (!cmd) LatchData |= HD44780->HD44780_Connection->RS_Pin;
  
   /* Add external connection if used */
@@ -169,25 +197,5 @@ static void HD44780_Write_8bit (uint8_t data, uint8_t cmd)
       | HD44780->HD44780_ExtConnection->EXT2_Pin;
   
   /* Send data */	
-  if (cmd != HD44780_RAW_DATA)
-  {
-    /* Send positive strobe */
-    LatchData |= HD44780->HD44780_Connection->E_Pin;
-    HD44780->Write16bData(LatchData);
-
-    /* Send negative strobe */
-    LatchData &= ~HD44780->HD44780_Connection->E_Pin;
-    HD44780->Write16bData(LatchData);
-  } 
-}
-
-static void HD44780_Cmd(HD44780_CMD_TypeDef CMD)
-{
-  if (HD44780->HD44780_Connection->BUS == BUS_4b)
-  {
-    HD44780_Write(((uint8_t)CMD & 0xF0) >> 4, 1);
-    HD44780_Write((uint8_t)CMD & 0x0F, 1);
-    return;
-  }
-  HD44780_Write(CMD, 1);
+	HD44780_WriteToBus(LatchData);
 }
