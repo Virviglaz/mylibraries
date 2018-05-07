dbd739de826f6dfc217655edf9cdbb98282000d7
Pavel <pavelnadein@gmail.com>
2018-05-07 23:41:42 +0300

    OrangePI I2C.c fixed

--- OrangePI/I2C.c
+++ OrangePI/I2C.c
@@ -1,31 +1,29 @@
 #include "I2C.h"
 
-static char * i2c_driver = null;
+static int i2c_handler = 0;
 
 I2C_ErrorTypeDef I2C_Init (const char * driver)
 {
-	if (driver == null) return NODRIVER;
+	if (driver == NULL) return NODRIVER;
 	
-	if (i2c_driver) return ALREADYOPENED;
+	if (i2c_handler) return ALREADYOPENED;
 	
-	if (open(driver, O_RDWR) < 0) return OPENFAILED;
-	
-	i2c_driver = driver;
+	if ((i2c_handler = open(driver, O_RDWR)) < 0) return OPENFAILED;
 	
 	return SUCCESS;
 }
 
 I2C_ErrorTypeDef I2C_Read (uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t * buf, uint16_t size)
 {
-	if (i2c_driver == null) return NODRIVER;
+	if (i2c_handler == 0) return NODRIVER;
 	
-	if (ioctl(i2c_driver, I2C_SLAVE, addr) < 0)
+	if (ioctl(i2c_handler, I2C_SLAVE, addr) < 0)
 		return NOACCESS;
 	
-	if (write(i2c_driver, reg, reglen) != reglen)
+	if (write(i2c_handler, reg, reglen) != reglen)
 		return READERROR;
 	
-	if (read(i2c_driver, buf, size) != size)
+	if (read(i2c_handler, buf, size) != size)
 		return READERROR;
 	
 	return SUCCESS;
@@ -33,15 +31,15 @@ I2C_ErrorTypeDef I2C_Read (uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t 
 
 I2C_ErrorTypeDef I2C_Write (uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t * buf, uint16_t size)
 {
-	if (i2c_driver == null) return NODRIVER;
+	if (i2c_handler == 0) return NODRIVER;
 	
-	if (ioctl(i2c_driver, I2C_SLAVE, addr) < 0)
+	if (ioctl(i2c_handler, I2C_SLAVE, addr) < 0)
 		return NOACCESS;
 	
-	if (write(i2c_driver, reg, reglen) != reglen)
+	if (write(i2c_handler, reg, reglen) != reglen)
 		return READERROR;
 	
-	if (write(i2c_driver, buf, size) != size)
+	if (write(i2c_handler, buf, size) != size)
 		return READERROR;
 	
 	return SUCCESS;
