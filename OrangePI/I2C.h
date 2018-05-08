f5ca8590efd0d969b8d90f3f17701073da671a51
Pavel <pavelnadein@gmail.com>
2018-05-08 22:26:19 +0300

    updated orange pi i2c driver

--- OrangePI/I2C.h
+++ OrangePI/I2C.h
@@ -10,17 +10,19 @@
 typedef enum
 {
 	SUCCESS = 0,
-	OPENFAILED,
-	NOACCESS,
 	READERROR,
 	WRITEERROR,
-	NOTOPENED,
-	ALREADYOPENED,
-	NODRIVER
+	NODRIVER,
+	NOTFOUND
 }I2C_ErrorTypeDef;
 
-I2C_ErrorTypeDef I2C_Init (const char * driver);
-I2C_ErrorTypeDef I2C_Read (uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t * buf, uint16_t size);
-I2C_ErrorTypeDef I2C_Write (uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t * buf, uint16_t size);
+I2C_ErrorTypeDef I2C_Read (char * driver, uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t * buf, uint16_t size);
+I2C_ErrorTypeDef I2C_Write (char * driver, uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t * buf, uint16_t size);
+
+static const struct
+{
+	I2C_ErrorTypeDef (* Read) (char * driver, uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t * buf, uint16_t size);
+	I2C_ErrorTypeDef (* Write) (char * driver, uint8_t addr, uint8_t * reg, uint8_t reglen, uint8_t * buf, uint16_t size);
+}I2C = {I2C_Read, I2C_Write};
 
 #endif
