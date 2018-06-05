fd66e85b3e3a68684ace8a17b27049e00810f9e4
Pavel Nadein <pavelnadein@gmail.com>
2018-06-05 13:26:24 +0000

    mymath.c edited online with Bitbucket

--- Common/mymath.c
+++ Common/mymath.c
@@ -1,9 +1,9 @@
-
+#include "mymath.h"
 
 float Linearize(float value, float x1, float y1, float x2, float y2)
 {
     float k = (y2 - y1) / (x2 - x1);
     float b = y1 - k * x1;
     
-    return value * k + b;
+    return (value < x1 || value > x2) ? 0 : value * k + b;
 }
