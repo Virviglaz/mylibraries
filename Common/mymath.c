e1d9fcb0f7c9be255aea28bbab6470050dfad36e
Pavel <pavelnadein@gmail.com>
2018-06-05 19:10:47 +0300

    mymath -> added header

--- Common/mymath.c
+++ Common/mymath.c
@@ -5,5 +5,5 @@ float Linearize(float value, float x1, float y1, float x2, float y2)
     float k = (y2 - y1) / (x2 - x1);
     float b = y1 - k * x1;
     
-    return (value < x1 || value > x2) ? 0 : value * k + b;
+    return value * k + b;
 }
