3185df19e5716a5fdf1c5c4ddd34dd70d2e91ba9
Pavel <pavelnadein@gmail.com>
2018-06-14 18:46:32 +0300

    added Altium

---
+++ Altium/rename_script.bat
@@ -0,0 +1,8 @@
+ren *.gbl Bottom.gbr
+ren *.gtl Top.gbr
+ren *.gts MaskTop.gbr
+ren *.gbs MaskBottom.gbr
+ren *.gm8 Outline.gbr
+ren *.gbo BottomOverlay.gbr
+ren *.gto TopOverlay.gbr
+ren *.drl Plated.drl
