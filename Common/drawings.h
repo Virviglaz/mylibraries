9bafe2a2ec333865dc625401d704c9ec8411abbe
Pavel <pasha-net@narod.ru>
2018-12-27 11:19:31 +0200

    added drawings

---
+++ Common/drawings.h
@@ -0,0 +1,35 @@
+#ifndef DRAWINGS_H
+
+#include <stdint.h>
+
+#ifndef COLOR_DEPTH
+	#define COLOR_DEPTH uint32_t
+#endif // !COLOR_DEPTH
+
+#ifndef FONT_SIZE_T
+	#define FONT_SIZE_T uint8_t
+#endif // !FONT_SIZE_T
+
+typedef struct
+{
+	void(*pixel) (uint16_t x, uint16_t y, COLOR_DEPTH color);
+	void (*line) (int16_t x0, int16_t y0, int16_t x1, int16_t y1, COLOR_DEPTH color);
+	void(*hline) (uint16_t x0, uint16_t y0, uint16_t x1, COLOR_DEPTH color);
+	void(*vline) (uint16_t x0, uint16_t y0, uint16_t y1, COLOR_DEPTH color);
+	void(*circle)(int16_t x, int16_t y, int16_t r, COLOR_DEPTH color);
+	void(*filled_circle)(int16_t x, int16_t y, int16_t r, COLOR_DEPTH color);
+	void (*cross)(uint16_t x0, uint16_t y0, uint16_t size, COLOR_DEPTH color);
+	uint16_t(*print)(uint16_t x0, uint16_t y0, const char * text);
+}drawings_t;
+
+typedef struct
+{
+	FONT_SIZE_T FontXsize, FontYsize;
+	const uint8_t * Font;
+	COLOR_DEPTH FontColor, BackColor;
+}font_t;
+
+drawings_t * drawings_init(void(*set_window_func) (uint16_t, uint16_t, uint16_t, uint16_t),
+	void(*fill_window_func) (COLOR_DEPTH), font_t * font);
+
+#endif // !DRAWINGS_H
