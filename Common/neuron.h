044274989e779e446750d2ca105c79155c00b626
Pavel <pasha-net@narod.ru>
2019-01-16 22:06:34 +0200

    neuron: changed activation function

--- Common/neuron.h
+++ Common/neuron.h
@@ -38,12 +38,11 @@ typedef struct
 	net_t * (*create_net)(u_t * neurons_per_layer, u_t layer_count, f_t * input, u_t input_count);
 	void (*release_net)(net_t * net);
 	f_t * (*get_result)(net_t * net);
-	f_t (*weight_get_func)(u_t layer, u_t num);
-	f_t (*result_calc_func)(f_t input);
+	f_t (*weight_get_func)(u_t layer, u_t neuron, u_t input);
+	f_t (*trigger_func)(f_t input);
 	void (*update_weights)(void);
 }neutron_network_t;
 
 neutron_network_t * get_network(void);
-void set_network(neutron_network_t * network);
 
 #endif //NEURON_H
