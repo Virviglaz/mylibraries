a860b43b51c3b81d79198489c1ae2d71b70fa201
Pavel <pasha-net@narod.ru>
2019-09-11 23:48:27 +0200

    removed altium libs
    Signed-off-by: Pavel <pasha-net@narod.ru>

--- Altium/rename_script.bat
+++
@@ -1,8 +0,0 @@
-ren *.gbl Bottom.gbr
-ren *.gtl Top.gbr
-ren *.gts MaskTop.gbr
-ren *.gbs MaskBottom.gbr
-ren *.gm8 Outline.gbr
-ren *.gbo BottomOverlay.gbr
-ren *.gto TopOverlay.gbr
-ren *.drl Plated.drl
