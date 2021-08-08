47be77299a65378efd2460392b68f75b75ef570d
Pavel Nadein <pavelnadein@gmail.com>
2021-08-08 19:15:18 +0000

    [server] API updated. Connect, disconnect and error callbacks added
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- Linux/server.h
+++ Linux/server.h
@@ -5,11 +5,17 @@
 
 typedef struct server_t *server_t;
 typedef struct client_t *client_t;
-typedef void(*rcv_handler)(client_t server, void *data, size_t size);
 
-server_t server_start(int port, rcv_handler handler, int size, void *user);
+typedef struct {
+	int (*connected)(client_t client);
+	int (*receive)(client_t client, void *data, size_t size);
+	void (*disconnected)(client_t client);
+	void (*error)(const char *str, int error, void *user);
+} server_event_t;
+
+server_t server_start(int port, server_event_t *ops, int size, void *user);
 int send_to_client(client_t client, void *msg, size_t size);
-char *get_client_ip(client_t client);
+const char *get_client_ip(client_t client);
 int get_client_port(client_t client);
 void *get_user_data(client_t client);
 int server_stop(server_t server);
