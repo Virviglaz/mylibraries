01ef09b97905d69f41df92bb74894e5d43350fde
Pavel Nadein <pavelnadein@gmail.com>
2022-02-15 23:25:53 +0100

    [ESP32] add rotary encoder driver
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

---
+++ ESP32/inc/esp_encoder.h
@@ -0,0 +1,75 @@
+/*
+ * This file is provided under a MIT license.  When using or
+ * redistributing this file, you may do so under either license.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 Pavel Nadein
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ESP32 rotary encoder driver
+ *
+ * Contact Information:
+ * Pavel Nadein <pavelnadein@gmail.com>
+ */
+
+#ifndef __ENCODER_H__
+#define __ENCODER_H__
+
+#include <stdint.h>
+
+/* ESP32 drivers */
+#include "driver/gpio.h"
+
+class encoder
+{
+public:
+	encoder(gpio_num_t a, gpio_num_t b);
+	~encoder();
+	int32_t get_value();
+	void set_step(int32_t new_step = 1);
+	void invert();
+
+private:
+	static void isr_a(void *params);
+	static void isr_b(void *params);
+
+	bool init_done = false;
+	gpio_num_t enc_a;
+	gpio_num_t enc_b;
+	int32_t value = 0;
+	int32_t step = 1;
+	enum { ENC_A, ENC_B } prev;
+	uint8_t seq = 0;
+};
+
+#endif /* __ENCODER_H__ */
---
+++ ESP32/src/esp_encoder.cpp
@@ -0,0 +1,150 @@
+/*
+ * This file is provided under a MIT license.  When using or
+ * redistributing this file, you may do so under either license.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 Pavel Nadein
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ESP32 rotary encoder driver
+ *
+ * Contact Information:
+ * Pavel Nadein <pavelnadein@gmail.com>
+ */
+
+#include "esp_encoder.h"
+#include "esp_err.h"
+
+static esp_err_t gpio_config(gpio_num_t g)
+{
+	esp_err_t res;
+
+	res = gpio_reset_pin(g);
+	if (res)
+		return res;
+
+	res = gpio_set_direction(g, GPIO_MODE_INPUT);
+	if (res)
+		return res;
+
+	res = gpio_set_pull_mode(g, GPIO_PULLUP_ONLY);
+	if (res)
+		return res;
+
+	res = gpio_pullup_en(g);
+	if (res)
+		return res;
+
+	res = gpio_set_intr_type(g, GPIO_INTR_POSEDGE);
+
+	return res;
+}
+
+IRAM_ATTR void encoder::isr_a(void *params)
+{
+	encoder *e = static_cast<encoder *>(params);
+	
+	if (e->prev == ENC_A)
+		return;
+	e->prev = ENC_A;
+
+	e->seq <<= 2;
+	e->seq |= gpio_get_level(e->enc_b) << 1;
+	e->seq &= 0xF;
+
+	if (e->seq == 4)
+		e->value += e->step;
+}
+
+IRAM_ATTR void encoder::isr_b(void *params)
+{
+	encoder *e = static_cast<encoder *>(params);
+
+	if (e->prev == ENC_B)
+		return;
+	e->prev = ENC_B;
+
+	e->seq <<= 2;
+	e->seq |= gpio_get_level(e->enc_a) << 0;
+	e->seq &= 0xF;
+
+	if (e->seq == 8)
+		e->value -= e->step;
+}
+
+#define ERR_RTN(x)	if (x) return;
+encoder::encoder(gpio_num_t a, gpio_num_t b)
+{
+	enc_a = a;
+	enc_b = b;
+
+	ERR_RTN(gpio_install_isr_service(0));
+	ERR_RTN(gpio_config(enc_a));
+	ERR_RTN(gpio_config(enc_b));
+	ERR_RTN(gpio_isr_handler_add(enc_a, encoder::isr_a, this));
+	ERR_RTN(gpio_isr_handler_add(enc_b, encoder::isr_b, this));
+
+	init_done = true;
+}
+
+encoder::~encoder()
+{
+	gpio_isr_handler_remove(enc_a);
+
+	gpio_reset_pin(enc_a);
+	gpio_reset_pin(enc_b);
+}
+
+/*
+ * Read the current value
+ * @return Returns the actual value.
+ */
+int32_t encoder::get_value()
+{
+	return value;
+}
+
+/*
+ * Define the encoder step size
+ * @param new_step	Set the single step value.
+ */
+void encoder::set_step(int32_t new_step)
+{
+	step = new_step;
+}
+
+/* Invert the encoder direction */
+void encoder::invert()
+{
+	step = -step;
+}
