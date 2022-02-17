afab9540fbd270d72b7af036703496ebd92f5ca6
Pavel Nadein <pavelnadein@gmail.com>
2022-02-17 23:58:13 +0100

    [ESP32] add i2c driver
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

---
+++ ESP32/inc/esp32_i2c.h
@@ -0,0 +1,80 @@
+/*
+ * This file is provided under a MIT license.  When using or
+ * redistributing this file, you may do so under either license.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 Pavel Nadein
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ESP32 i2c driver
+ *
+ * Contact Information:
+ * Pavel Nadein <pavelnadein@gmail.com>
+ */
+
+#ifndef __ESP32_I2C_H__
+#define __ESP32_I2C_H__
+
+#include <stdint.h>
+#include "driver/i2c.h"
+
+enum i2c_freq {
+	I2C_FREQ_100K = 100000UL,
+	I2C_FREQ_400K = 400000UL,
+	I2C_FREQ_800K = 800000UL,
+};
+
+class i2c
+{
+public:
+	i2c(int sda_pin, int scl_pin, enum i2c_freq freq = I2C_FREQ_100K,
+		bool pullup = true, int bus_num = 0);
+	~i2c();
+
+	esp_err_t write(uint8_t addr, uint8_t *reg, uint16_t reg_size,
+	uint8_t *buf, uint16_t size);
+
+	esp_err_t write_reg(uint8_t addr, uint8_t reg,
+		uint8_t *buf, uint16_t size);
+
+	esp_err_t read_reg(uint8_t addr, uint8_t reg,
+		uint8_t *buf, uint16_t size);
+
+	const char *get_bus_name();
+private:
+	i2c_port_t bus;
+	i2c_cmd_handle_t handle = NULL;
+	char bus_name[5];
+};
+
+#endif /* __ESP32_I2C_H__ */
