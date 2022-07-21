b49dde2b1ed8035286b4028798b728d3964fd67a
Pavel Nadein <pavelnadein@gmail.com>
2022-07-21 16:55:40 +0000

    [log] add printf attribute
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- Linux/log.h
+++ Linux/log.h
@@ -2,6 +2,10 @@
 #define __LOG_H__
 
 void get_timestamp(char *dest, size_t size);
-void log_to_file(const char *filename, const char *format, ...);
+void log_to_file(const char *filename, const char *format, ...)
+#ifdef __GNUC__
+          __attribute__ (( format( printf, 2, 3 ) ))
+#endif
+;
 
 #endif /* __LOG_H__*/
