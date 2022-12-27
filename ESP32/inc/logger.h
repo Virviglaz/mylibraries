0ec1b42d6920c29af01fe93af7d9d1e4236a8d68
Pavel Nadein <pavelnadein@gmail.com>
2022-12-27 09:56:19 +0100

    [ESP32 remote logger] add flexible configuration options and token filter
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- ESP32/inc/logger.h
+++ ESP32/inc/logger.h
@@ -1,8 +1,50 @@
 #ifndef __LOGGER_H__
 #define	__LOGGER_H__
 
-#include "esp_log.h"
+#include <stdint.h>
 #include <stdbool.h>
+
+/**
+ * @brief Default init struct for remote logger.
+ *
+ * @param x		Server IP address string.
+ * @param y		Server port number.
+ */
+#define LOGGER_DEFAULT_INIT(x, y)	{ \
+						.server = (x), \
+						.port = (y), \
+						.message_queue_size = 1000, \
+						.server_poll_timeout = 10000, \
+						.printf_buffer_size = 256, \
+						.minimum_free_memory = 0x6000, \
+						.task_heap_size = 0x1000, \
+					}
+
+/**
+ * @brief Logger parameters struct.
+ */
+typedef struct {
+	/** Server IP address string. */
+	const char *server;
+
+	/** Server port number. */
+	uint32_t port;
+
+	/** Message queue size (number of message can wait in the queue)*/
+	uint32_t message_queue_size;
+
+	/** How offten check for server availability in ms. */
+	uint32_t server_poll_timeout;
+
+	/** Maximum log line in bytes. */
+	uint32_t printf_buffer_size;
+
+	/** Minimum free memory of the system before queue will become full. */
+	uint32_t minimum_free_memory;
+
+	/** Handler task head size. */
+	uint32_t task_heap_size;
+} logger_init_t;
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,21 +60,19 @@ void switch_printf(bool remote);
 /**
  * Initialize message queue and start handling task for message pool.
  *
- * @param[in] server	Pointer to server IP address string.
- * @param[in] port	Server port number.
+ * @param[in] logger_init_t	Pointer to init struct.
  */
-void message_logger_init(const char *server, uint32_t port);
+void message_logger_init(logger_init_t *init);
 
 /**
  * @brief Init message queue and redirect all output to remote server.
  * 
- * @param[in] server	Pointer to server IP address string.
- * @param[in] port	Server port number.
+ * @param[in] logger_init_t	Pointer to init struct.
  *
  * @note	This doesn't initialize the handling task. All messages will
  *		be stored in the queue until wifi is connected.
  */
-void message_logger_early_init(const char *server, uint32_t port);
+void message_logger_early_init(logger_init_t *init);
 
 #ifdef __cplusplus
 }
