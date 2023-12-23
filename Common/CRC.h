956d86ca4313d7008257bb1d7a7dc629f7f6ebfa
Pavel Nadein <pavelnadein@gmail.com>
2023-12-23 11:32:59 +0100

    [CRC] fix header
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- Common/CRC.h
+++ Common/CRC.h
@@ -1,9 +1,9 @@
-#ifndef CRC_H
-#define CRC_H
+#ifndef __CRC_H__
+#define __CRC_H__
 #include <stdint.h>
 
 uint32_t crc32(uint8_t *buf, uint32_t len);
 uint16_t crc16(uint8_t *buf, uint16_t len);
 uint8_t Crc8Dallas(uint8_t *buf, uint16_t len);
 uint8_t Crc8(uint8_t *buf, uint16_t len);
-#endif
+#endif /* __CRC_H__ */
