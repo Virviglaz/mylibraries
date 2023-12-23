45bbc40a4b47053c639e5ceeed9f0bfe55311f6e
Pavel Nadein <pavelnadein@gmail.com>
2023-12-23 12:03:18 +0100

    [CRC] rename CRC -> crc_gereric
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- Common/crc_generic.h
+++ Common/crc_generic.h
@@ -1,9 +1,9 @@
-#ifndef __CRC_H__
-#define __CRC_H__
+#ifndef __CRC_GENERIC_H__
+#define __CRC_GENERIC_H__
 #include <stdint.h>
 
 uint32_t crc32(uint8_t *buf, uint32_t len);
 uint16_t crc16(uint8_t *buf, uint16_t len);
 uint8_t Crc8Dallas(uint8_t *buf, uint16_t len);
 uint8_t Crc8(uint8_t *buf, uint16_t len);
-#endif /* __CRC_H__ */
+#endif /* __CRC_GENERIC_H__ */
