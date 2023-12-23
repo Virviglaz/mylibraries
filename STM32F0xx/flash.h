750f7de8f5fe6057e78cb6a427de52672f0887a4
Pavel Nadein <pavelnadein@gmail.com>
2023-12-23 11:45:41 +0100

    [flash] move typedef implementation in this header
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- STM32F0xx/flash.h
+++ STM32F0xx/flash.h
@@ -3,11 +3,19 @@
 
 #include "stm32f0xx.h"                  // Device header
 #include "stm32f0xx_flash.h"
-#include "bsp.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+
+#define FLASH_PAGE_SIZE				0x400
+
+typedef const struct
+{
+	uint32_t StartAddress;
+	uint32_t Size;
+	uint32_t Pages;
+} flash_t;
 
 FLASH_Status FlashPagesErase (uint32_t Address, uint8_t Pages, const uint32_t PageSize);
 FLASH_Status FlashErase (uint32_t Address, uint32_t Size, const uint32_t PageSize);
